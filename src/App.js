21dbae392765def0a32aedf383d1079fb6a6a293
Arpit <arpitjaiswal9526@gmail.com>
2024-04-23 15:25:51 +0530

    Learning day12

--- src/App.js
+++ src/App.js
@@ -3,12 +3,29 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, { useState } from 'react'
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toglemode = () =>{
+    if (mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = 'black'
+      document.body.style.color = 'white'
+    }
+    
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = 'white'
+      document.body.style.color = 'black'
+    }
+  }
+
   return (
     <>
     {/* <Navbar title="TextUtils" aboutText="About Us" /> */}
-    <Navbar title="TextUtils" />
+    <Navbar title="TextUtils" modee={mode} tooglemode={toglemode} />
     {/* <Navbar /> */}
 
     <div className='container my-4'>
